e53af5bb62e19f37041b0ef5b70b726105c06a58
programming.mohit <programming.mohit@gmail.com>
2022-07-20 02:18:34 +0530

    added scraping functionality from astronomy.com

--- news/models.py
+++ news/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Headline(models.Model):
 
       title = models.CharField(max_length=200)
-      company = models.TextField()
+      image = models.TextField()
       """URLField(null=True, blank=True)"""
-      location = models.TextField()
+      snippet = models.TextField()
       
       def __str__(self):
           return self.title
--- news/views.py
+++ news/views.py
@@ -4,40 +4,25 @@ from bs4 import BeautifulSoup as BSoup
 from news.models import Headline    
 
 def scrape(request):
+
+    URL = "https://astronomy.com/news"
+    page = requests.get(URL)
+    soup = BSoup(page.content, "html.parser")
+    results = soup.find("div", class_="latestNews")
+    news_items = results.find_all("div", class_="dataItem")
+
+
+    for news_element in news_items:
+        title_element = news_element.find("div", class_="content").find("h2").find("a")
+        image_element = news_element.find("div", class_="thumbnail").find("a").find("img")['src']
+        snippet_element = news_element.find("div", class_="content").find("div", class_="snippet")
+        title_text = str(title_element.text)
+        image_text = str(image_element)
+        snippet_text = str(snippet_element.text)
+
+        #Code for stoing into database
+        Headline.objects.create(title=title_text, image=image_text, snippet=snippet_text)
     return redirect("../")
 
 def news_list(request):
-    
-    URL = "https://realpython.github.io/fake-jobs/"
-    page = requests.get(URL)
-    soup = BSoup(page.content, "html.parser")
-    results = soup.find(id="ResultsContainer")
-    job_results = results.find_all("div", class_="card-content")
-    job_dict = {
-        "title": "",
-        "company": "",
-        "location": "",
-    }
-
-    job_arr = []
-
-    for job_element in job_results:
-        job_dict = {}
-        title_element = job_element.find("h2", class_="title")
-        company_element = job_element.find("h3", class_="company")
-        location_element = job_element.find("p", class_="location")
-        """job_dict["title"] = str(title_element.text)
-        job_dict["company"] = str(company_element.text)
-        job_dict["location"] = str(location_element.text)
-        job_arr.append(job_dict)"""
-        title_text = str(title_element.text)
-        company_text = str(company_element.text)
-        location_text = str(location_element.text)
-
-        #Code for stoing into database
-        Headline.objects.create(title=title_text, company=company_text, location=location_text)
-
-    context = {
-        "job_arr": job_arr,
-    }
-    return render(request, "home.html", context)
+    return render(request, "home.html")
